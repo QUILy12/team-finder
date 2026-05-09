929aa029e81d4e5e6e02e8073fe5f2b0123e1754
QUILy12 <cryptofed16@gmail.com>
2026-05-09 20:07:06 +0300

    Fix app configs and regenerate migrations

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,7 @@
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
-    verbose_name = "РџРѕР»СЊР·РѕРІР°С‚РµР»Рё"
+    verbose_name = "Пользователи"
